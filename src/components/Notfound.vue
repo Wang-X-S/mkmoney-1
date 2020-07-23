c4cf4eb13bbc38ae619cba207f8688241e589b84
Wang-XS <Wang-XS@hotmail.com>
2020-07-23 11:49:49 +0800

    添加404页面

---
+++ src/components/Notfound.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div>当前页面不存在,请检查网址是否正确
+        </div>
+        <div id="back">
+            <router-link to="/money">返回首页</router-link>
+        </div>
+    </div>
+
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Notfound'
+  };
+</script>
+
+<style scoped lang="scss">
+
+</style>
