c56a9b7136f743a494675cf89a1449bc72828504
Wang-XS <Wang-XS@hotmail.com>
2020-07-25 13:08:05 +0800

    active-class

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -15,14 +15,14 @@
 
 <style scoped lang="scss">
     .nav-wrapper {
-        border: 1px solid green;
+
         display: flex;
         flex-direction: column;
         height: 100vh
     }
 
     .content {
-        border:1px solid blue;
+
         overflow: auto;
         flex-grow: 1;
     }
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,21 +1,50 @@
 <template>
-    <div class="nav" data-v-x1>
-        <router-link to="/money">记账</router-link>
-        |
-        <router-link to="/labels">标签</router-link>
-        |
-        <router-link to="/statistics">统计</router-link>
-    </div>
+    <nav >
+        <router-link to="/money" class="item" active-class="selected">
+            <Icon name="money"/>
+            记账</router-link>
+
+        <router-link to="/labels" class="item" active-class="selected">
+            <Icon name="labels"/>
+            标签</router-link>
+
+        <router-link to="/statistics" class="item" active-class="selected">
+            <Icon name="statistics"/>
+            统计</router-link>
+    </nav>
 </template>
 
 <script lang="ts">
+  const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+  try {importAll(require.context('../assets/icons', true, /\.svg$/));} catch (error) {console.log(error);}
+
+
   export default {
     name: 'Nav'
   };
 </script>
 
 <style scoped lang="scss">
- .nav[data-v-x1]{
-     border:1px solid red;
+ nav{
+     display: flex;
+     box-shadow: 0 0 3px rgba(0,0,0,0.25);
+    flex-direction: row;
+
+     >.item{
+         padding:2px 0;
+        width:33.333333%; //三分
+         display:flex;
+         justify-content:center;
+         align-items:center;
+        flex-direction:column; //icon上移
+        .icon{
+            width:32px;
+            height:32px;
+        }
+     }
+    >.item.selected{
+        color:red;
+    }
  }
+
 </style>
